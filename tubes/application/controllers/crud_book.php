1ad813b0d809f6fc16e15dfcfc859a8459807d84
ariqmusyaffaramadhani <ariqmr123@gmail.com>
2019-11-26 22:17:44 +0700

    add Controller + model buku

---
+++ tubes/application/controllers/crud_book.php
@@ -0,0 +1,26 @@
+<?php
+
+  class C_Buku extends CI_Controller  {
+    public function __construct() {
+      parent::__construct();
+      $this->load->model('M_Buku');
+    }
+
+    public function index(){
+        //$data['detail_buku']=$this->Buku_M->get_all();
+        $this->load->view('crudAdm/addBook_V');
+    }
+
+    public function view(){
+      $this->load->view('halPencarian');
+    }
+    public function search(){
+      // $keyword = $this->input->post('keyword');
+      $data=$this->M_Buku->get_buku_keyword();
+      $this->load->view('lapPencarian',$data);
+
+    }
+
+  }
+
+  ?>
--- tubes/application/models/M_Buku.php
+++ tubes/application/models/M_Buku.php
@@ -6,12 +6,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       return $this->db->get('detail_buku')->result();
     }
     public function get_buku_keyword(){
-      $keyword = $this->input->post('keyword',true);
-      $this->db->select('*');
-      $this->db->from('detail_buku');
-      $this->db->like('judul_buku',$keyword);
-      //
-      // $query = $this->db->get();
+      // $keyword = $this->input->post('keyword',true);
+      // $this->db->select('*');
+      // $this->db->from('detail_buku');
+      // $this->db->like('judul_buku',$keyword);
+      // //
+      // // $query = $this->db->get();
       return $this->db->get()->result_array();
     }
 
