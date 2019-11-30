756429c3fb8b49765694c1a32fb30ef15d5fd998
ariqmusyaffaramadhani <ariqmr123@gmail.com>
2019-11-30 23:11:33 +0700

    delete admin complete

--- tubes/application/views/homeAdmin.php
+++ tubes/application/views/homeAdmin.php
@@ -6,7 +6,7 @@
     <center>
         <a href="<?php echo site_url('crud_book_C/addBookV'); ?>" style="color: white;"><button class="adm_button">Tambah</button></a> <br>
         <a href="<?php echo site_url('crud_book_C/editbookV'); ?>" style="color: white;"><button class="adm_button">Ubah</button></a> <br>
-        <a href="<?php echo site_url('crud_book_C'); ?>" style="color: white;"><button class="adm_button">Hapus</button></a> <br>
+        <a href="<?php echo site_url('crud_book_C/delbookV'); ?>" style="color: white;"><button class="adm_button">Hapus</button></a> <br>
         <a href="<?php echo site_url('crud_book_C/viewbookV'); ?>" style="color: white;"><button class="adm_button">Daftar Buku</button></a>
     </center>
     </div>
